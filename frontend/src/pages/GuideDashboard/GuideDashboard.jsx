28ac86c6a81e10608dcc98654268e37e79f604c8
fahmida47 <fahmida.200547@gmail.com>
2026-08-22 13:46:14 +0600

    update guide dashboard

--- frontend/src/pages/GuideDashboard/GuideDashboard.jsx
+++ frontend/src/pages/GuideDashboard/GuideDashboard.jsx
@@ -1,21 +1,10 @@
 import "./GuideDashboard.css";
-
-import GuideSidebar from "./components/GuideSidebar";
-import GuideHeader from "./components/GuideHeader";
 import DashboardOverview from "./components/DashboardOverview";
 
 const GuideDashboard = () => {
   return (
-    <div className="guide-dashboard">
-      <GuideSidebar />
-
-      <div className="dashboard-content">
-        <GuideHeader />
-
-        <div className="dashboard-body">
-          <DashboardOverview />
-        </div>
-      </div>
+    <div className="dashboard-body">
+      <DashboardOverview />
     </div>
   );
 };
