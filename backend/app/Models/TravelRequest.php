284ebff096c6cf6564f24ab1aa69bd22343fcf80
fahmida47 <fahmida.200547@gmail.com>
2026-09-12 02:20:48 +0600

    refactor: update TravelRequest model

--- backend/app/Models/TravelRequest.php
+++ backend/app/Models/TravelRequest.php
@@ -10,59 +10,104 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class TravelRequest extends Model
 {
+
     protected $fillable = [
+
         'tourist_profile_id',
+
         'guide_profile_id',
+
         'guide_experience_id',
+
+        'experience_name',
+
         'destination',
+
         'travelers',
-        'travel_date',
+
+        'from_date',
+
+        'to_date',
+
         'amount',
+
         'request_details',
+
         'status',
+
     ];
 
+
     protected $casts = [
-        'travel_date' => 'date',
+
+        'from_date' => 'date',
+
+        'to_date' => 'date',
+
         'amount' => 'float',
+
         'travelers' => 'integer',
+
     ];
+
+
 
     public function tourist(): BelongsTo
     {
+
         return $this->belongsTo(
+
             TouristProfile::class,
+
             'tourist_profile_id'
+
         );
+
     }
+
+
 
     public function guide(): BelongsTo
     {
+
         return $this->belongsTo(
+
             GuideProfile::class,
+
             'guide_profile_id'
+
         );
+
     }
+
+
 
     public function experience(): BelongsTo
     {
+
         return $this->belongsTo(
+
             GuideExperience::class,
+
             'guide_experience_id'
+
         );
+
     }
 
-    /*
-    |--------------------------------------------------------------------------
-    | Booking
-    |--------------------------------------------------------------------------
-    */
+
 
     public function booking(): HasOne
     {
+
         return $this->hasOne(
+
             Booking::class,
+
             'travel_request_id'
+
         );
+
     }
+
 }
